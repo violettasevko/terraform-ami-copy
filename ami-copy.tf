11eb2e846c204d88c9f0e0c666212c0850554f3b
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-16 03:05:21 +0300

    ami fix

--- ami-copy.tf
+++ ami-copy.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_ami_copy" "encrypted_ami" {
   name              = "encrypted-ami"
   description       = "An encrypted ami"
-  source_ami_id     = "i-0422a40f350f68455"
+  source_ami_id     = "ami-07f16fb14274bfc76"
   source_ami_region = "eu-central-1"
   encrypted         = true
 
