2dd594aa12894fd3c0779af18f38ef2015c26e74
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-17 02:16:31 +0300

    ami for jenkins1

--- ami-copy.tf
+++ ami-copy.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_ami_copy" "encrypted_ami" {
-  name              = "encrypted-ami"
-  description       = "An encrypted ami"
-  source_ami_id     = "ami-07f16fb14274bfc76"
+resource "aws_ami_copy" "jenkins1" {
+  name              = "jenkins1"
+  description       = "jenkins1 ami"
+  source_ami_id     = "ami-0b83282d4c7adda4c"
   source_ami_region = "eu-central-1"
   encrypted         = true
 
-  tags = { Name = "encrypted-ami" }
+  tags = { Name = "jenkins1" }
 }
 
-data "aws_ami" "encrypted-ami" {
+data "aws_ami" "jenkins1" {
   most_recent = true
 
   owners = ["self"]
