9bd2ca549966ae37f7ad1a2b267a3e3a373d6b18
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-16 03:01:31 +0300

    refactoring

--- ami-copy.tf
+++ ami-copy.tf
@@ -2,44 +2,18 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["679593333241"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-focal-22.04-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-
-
-
-resource "aws_ami_copy" "ubuntu_encrypted_ami" {
-  name              = "ubuntu-encrypted-ami"
-  description       = "An encrypted root ami based off ${data.aws_ami.ubuntu.id}"
-  source_ami_id     = data.aws_ami.ubuntu.id
+resource "aws_ami_copy" "encrypted_ami" {
+  name              = "encrypted-ami"
+  description       = "An encrypted ami"
+  source_ami_id     = "i-0422a40f350f68455"
   source_ami_region = "eu-central-1"
   encrypted         = true
 
   tags = { Name = "ubuntu-encrypted-ami" }
 }
 
-
-
-
 data "aws_ami" "encrypted-ami" {
   most_recent = true
-
-  filter {
-    name   = "name"
-    values = [aws_ami_copy.ubuntu_encrypted_ami.id]
-  }
 
   owners = ["self"]
 }
