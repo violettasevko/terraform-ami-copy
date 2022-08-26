15b301219208d4da5a47a17640c48736b4c7df76
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-27 01:08:29 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,7 @@
+locals {
+  bucket_name = "s3-bucket-1"
+  region      = "eu-central-1"
+}
 
 module "s3_bucket" {
   source = "../../"
@@ -5,30 +9,18 @@ module "s3_bucket" {
   bucket = local.bucket_name
 
   force_destroy       = true
-  acceleration_status = "Suspended"
-  request_payer       = "BucketOwner"
+  #acceleration_status = "Suspended"
+  #request_payer       = "BucketOwner"
 
-  tags = {
-    Owner = "Anton"
-  }
-
-  # Note: Object Lock configuration can be enabled only on new buckets
+    # Note: Object Lock configuration can be enabled only on new buckets
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object_lock_configuration
-  object_lock_enabled = true
-  object_lock_configuration = {
-    rule = {
-      default_retention = {
-        mode = "GOVERNANCE"
-        days = 1
-      }
-    }
-  }
+  object_lock_enabled = false
 
   # Bucket policies
-  attach_policy                         = true
-  policy                                = data.aws_iam_policy_document.bucket_policy.json
-  attach_deny_insecure_transport_policy = true
-  attach_require_latest_tls_policy      = true
+  # attach_policy                         = true
+  # policy                                = data.aws_iam_policy_document.bucket_policy.json
+  # attach_deny_insecure_transport_policy = true
+  # attach_require_latest_tls_policy      = true
 
   # S3 bucket-level Public Access Block configuration
   block_public_acls       = true
@@ -38,187 +30,23 @@ module "s3_bucket" {
 
   # S3 Bucket Ownership Controls
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_ownership_controls
-  control_object_ownership = true
-  object_ownership         = "BucketOwnerPreferred"
+  control_object_ownership = false
+  #object_ownership         = "BucketOwnerPreferred"
 
-  expected_bucket_owner = data.aws_caller_identity.current.account_id
+  #expected_bucket_owner = data.aws_caller_identity.current.account_id
 
-  acl = "private" # "acl" conflicts with "grant" and "owner"
-
-  logging = {
-    target_bucket = module.log_bucket.s3_bucket_id
-    target_prefix = "log/"
-  }
+  #acl = "private" # "acl" conflicts with "grant" and "owner"
 
   versioning = {
-    status     = true
-    mfa_delete = false
+    status     = false
   }
 
-  website = {
-    # conflicts with "error_document"
-    #        redirect_all_requests_to = {
-    #          host_name = "https://modules.tf"
-    #        }
-
-    index_document = "index.html"
-    error_document = "error.html"
-    routing_rules = [{
-      condition = {
-        key_prefix_equals = "docs/"
-      },
-      redirect = {
-        replace_key_prefix_with = "documents/"
-      }
-      }, {
-      condition = {
-        http_error_code_returned_equals = 404
-        key_prefix_equals               = "archive/"
-      },
-      redirect = {
-        host_name          = "archive.myhost.com"
-        http_redirect_code = 301
-        protocol           = "https"
-        replace_key_with   = "not_found.html"
-      }
-    }]
-  }
-
-  server_side_encryption_configuration = {
+  server_side_encryption = "aws:kms"
+   server_side_encryption_configuration = {
     rule = {
       apply_server_side_encryption_by_default = {
-        kms_master_key_id = aws_kms_key.objects.arn
+        kms_master_key_id = "arn:aws:kms:eu-central-1:935319783299:key/6de992a9-9816-4a1c-b011-ebd8e79061e8"
         sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  cors_rule = [
-    {
-      allowed_methods = ["PUT", "POST"]
-      allowed_origins = ["https://modules.tf", "https://terraform-aws-modules.modules.tf"]
-      allowed_headers = ["*"]
-      expose_headers  = ["ETag"]
-      max_age_seconds = 3000
-      }, {
-      allowed_methods = ["PUT"]
-      allowed_origins = ["https://example.com"]
-      allowed_headers = ["*"]
-      expose_headers  = ["ETag"]
-      max_age_seconds = 3000
-    }
-  ]
-
-  lifecycle_rule = [
-    {
-      id      = "log"
-      enabled = true
-
-      filter = {
-        tags = {
-          some    = "value"
-          another = "value2"
-        }
-      }
-
-      transition = [
-        {
-          days          = 30
-          storage_class = "ONEZONE_IA"
-          }, {
-          days          = 60
-          storage_class = "GLACIER"
-        }
-      ]
-
-      #        expiration = {
-      #          days = 90
-      #          expired_object_delete_marker = true
-      #        }
-
-      #        noncurrent_version_expiration = {
-      #          newer_noncurrent_versions = 5
-      #          days = 30
-      #        }
-    },
-    {
-      id                                     = "log1"
-      enabled                                = true
-      abort_incomplete_multipart_upload_days = 7
-
-      noncurrent_version_transition = [
-        {
-          days          = 30
-          storage_class = "STANDARD_IA"
-        },
-        {
-          days          = 60
-          storage_class = "ONEZONE_IA"
-        },
-        {
-          days          = 90
-          storage_class = "GLACIER"
-        },
-      ]
-
-      noncurrent_version_expiration = {
-        days = 300
-      }
-    },
-    {
-      id      = "log2"
-      enabled = true
-
-      filter = {
-        prefix                   = "log1/"
-        object_size_greater_than = 200000
-        object_size_less_than    = 500000
-        tags = {
-          some    = "value"
-          another = "value2"
-        }
-      }
-
-      noncurrent_version_transition = [
-        {
-          days          = 30
-          storage_class = "STANDARD_IA"
-        },
-      ]
-
-      noncurrent_version_expiration = {
-        days = 300
-      }
-    },
-  ]
-
-  intelligent_tiering = {
-    general = {
-      status = "Enabled"
-      filter = {
-        prefix = "/"
-        tags = {
-          Environment = "dev"
-        }
-      }
-      tiering = {
-        ARCHIVE_ACCESS = {
-          days = 180
-        }
-      }
-    },
-    documents = {
-      status = false
-      filter = {
-        prefix = "documents/"
-      }
-      tiering = {
-        ARCHIVE_ACCESS = {
-          days = 125
-        }
-        DEEP_ARCHIVE_ACCESS = {
-          days = 200
-        }
       }
     }
   }
