5ec53f3833b820da22602ca2cce7142fcb1b70d0
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-09-08 23:11:13 +0300

    Update oss.tf

--- oss.tf
+++ oss.tf
@@ -23,15 +23,15 @@ module "opensearch" {
 
   cluster_name    = local.cluster_name
   cluster_domain  = local.cluster_domain
-  cluster_version = "1.2"
+  cluster_version = "1.3"
 
   saml_entity_id        = local.saml_entity_id
   saml_metadata_content = data.http.saml_metadata.body
 
   indices = {
     example-index = {
-      number_of_shards   = 2
-      number_of_replicas = 1
+      number_of_shards   = 1
+      number_of_replicas = 0
     }
   }
 }
