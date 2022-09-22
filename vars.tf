2947af298f8cd648119ee90f2cb3eb8965881a95
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-09-22 15:39:20 +0300

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "region" {
+    default = "eu-central-1"
+}
+
+variable "ami_id" {
+  default = "ami-0ceb85bb30095410b"
+}
+
+variable "instance_type" {
+  default = "t4g.small"
+}
+
+variable "key_name" {
+    default = "hoi22"
+}
+
+variable "security_group_id" {
+    default = ["sg-019219fc96bf5722b", "sg-019219fc96bf57227"]
+}
+
+variable "subnet_id" {
+    default = ""
+}
