fad00714ebdb340b28e216c9aaf4c4c8b62afae1
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-10-11 00:04:30 +0300

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 18.0"
 
   cluster_name    = "my-cluster"
-  cluster_version = "1.25"
+  cluster_version = "1.23"
 
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
