849aada696041dff795bc8966280e43e5a3af373
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-10-11 17:25:16 +0300

    fix

--- eks/eks.tf
+++ eks/eks.tf
@@ -1,18 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-provider "kubernetes" {
-  host                   = module.eks_main.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_main.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks_main.cluster_id]
-  }
-}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
