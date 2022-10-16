cb362d4752ef671c6c7a671f19b6805c313de215
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-10-16 23:27:47 +0300

    Update variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -7,3 +7,8 @@ variable "cluster_version" {
  type    = string
  default = "1.23"
 }
+
+variable "subnet_ids" {
+ type    = list(string)
+ default = ["subnet-0101545d43a467f5b", "subnet-058e500f0b866e68b"]
+}
