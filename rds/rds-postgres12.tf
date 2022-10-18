51146c8f40a926c222b45bff680a1b03ea798486
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-10-18 19:42:45 +0300

    Update rds-postgres12.tf

--- rds/rds-postgres12.tf
+++ rds/rds-postgres12.tf
@@ -1,7 +1,7 @@
 module "db" {
   source  = "terraform-aws-modules/rds/aws"
 
-  identifier = "kong-pg11"
+  identifier = "kong-pg12"
 
   engine            = "postgres"
   engine_version    = "12.11"
