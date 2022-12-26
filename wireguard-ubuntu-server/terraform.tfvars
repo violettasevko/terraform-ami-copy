23dbae0c1fd433613e076ad73d3342d0f3e51471
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-12-26 04:10:37 +0300

    fix

--- wireguard-ubuntu-server/terraform.tfvars
+++ wireguard-ubuntu-server/terraform.tfvars
@@ -1,7 +1,7 @@
 ec2_name            = "vpn-wg-server"
-ami                 = "ami-0574da719dca65348" #ubuntu 22.04 x64
+ami_id                 = "ami-0574da719dca65348" #ubuntu 22.04 x64
 instance_type       = "t4g.small"
 region              = "us-east-1a" #virginia
-keykey_name         = "l1q_key"
+key_name         = "l1q_key"
 security_group_id   = ["sg-008d650fa174f6032"] #webssh
 subnet_id           = "subnet-008f1a536a8623779" #public 10.20.12.x
--- wireguard-ubuntu-server/variables.tf
+++ wireguard-ubuntu-server/variables.tf
@@ -1,35 +1,42 @@
 variable "ec2_name" {
-  description   = "Name for EC2 instance"
-  type          = string
+    description = "Name for EC2 instance"
+    type        = string
+    default     = ""  
 }
 
 variable "ami_id" {
     description = "AMI id for instance"
     type        = string
+    default     = "" 
 }
 
 variable "instance_type" {
     description = "Type of EC2 instance"
     type        = string
+    default     = "" 
 }
 
 variable "region" {
     description = "Region for EC2 instance"
     type        = string
+    default     = "" 
 }
 
 variable "key_name" {
     description = "Name of key for EC2 instance"
     type        = string
+    default     = "" 
 }
 
 variable "security_group_id" {
     description = "Name of key for EC2 instance"
     type        = list(string)
+    default     = "" 
 }
 
 variable "subnet_id" {
     description = "Name of key for EC2 instance"
     type        = string
+    default     = "" 
 }
 
